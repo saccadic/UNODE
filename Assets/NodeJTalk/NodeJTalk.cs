e165042ae86b1387c368b76f7ed1f5a97e4bc46e
Savant_Cat <scanf@me.com>
2014-11-09 22:56:14 +0900

    色々テスト

--- Assets/NodeJTalk/NodeJTalk.cs
+++ Assets/NodeJTalk/NodeJTalk.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
+using UnityEditor;
+using System;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 public class NodeJTalk : MonoBehaviour {
 	public Unode unode;
@@ -8,7 +11,7 @@ public class NodeJTalk : MonoBehaviour {
 	public string Voice;
 	public int Sampling;
 	public string file;
-	public string text;
+	[Multiline(10)]public string text;
 
 
 	// Use this for initialization
@@ -23,11 +26,21 @@ public class NodeJTalk : MonoBehaviour {
 			unode.sound = false;
 		}
 		if(Input.GetMouseButtonDown(0)){
-			unode.message(3,"\"dic\":\""+Dictionary+"\","+
-			              "\"voice\":\""+Voice     +"\","+
-			             "\"sample\":"  +Sampling  +  ","+
-			               "\"file\":\""+file      +"\","+
-			               "\"text\":\""+text      +"\"");
+			send(text);
 		}		
+		if(Input.GetMouseButtonDown(1)){
+			send("現在時刻は" + DateTime.Now.ToString("yyyy年MM月dd日 HH時mm分ss秒") + "です。");
+		}	
 	}
+
+	void send(string str){
+		str = str.Replace ("\n"," ");
+		unode.message(3,"\"dic\":\""+Dictionary+"\","+
+		              "\"voice\":\""+Voice     +"\","+
+		              "\"sample\":" +Sampling  +  ","+
+		              "\"file\":\"" +file      +"\","+
+		              "\"text\":\"" +str       +"\"");
+	}
+
+
 }
